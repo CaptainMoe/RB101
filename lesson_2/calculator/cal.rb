595f1a9fab547c4a14d36f6b79b5e38766edb9a0
CaptainMoe <mohamedaaa1997@gmail.com>
2019-03-03 07:48:22 -0500

    calculator

---
+++ lesson_2/calculator/cal.rb
@@ -0,0 +1,89 @@
+require 'yaml'
+
+def prompt(message)
+  Kernel.puts("=>#{message}")
+end
+
+def valid_number?(num)
+  num.to_i.to_s == num
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+loop do
+  name = gets.chomp.capitalize
+  break if !name.empty?
+  prompt(MESSAGES['valid_name'])
+end
+
+loop do
+  number1 = nil
+  loop do
+    prompt("What is your first number?")
+    number1 = Kernel.gets().chomp()
+
+    break if valid_number?(number1)
+    prompt("Invalid number")
+  end
+
+  number2 = nil
+  loop do
+    prompt("What is your second number?")
+    number2 = Kernel.gets().chomp()
+
+    break if valid_number?(number2)
+    prompt("Invalid number")
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+    break if Array('1'..'4').include?(operator)
+    prompt("Invalid operator. Must choose 1, 2 ,3 or 4")
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers")
+  result = case operator
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
+
+  prompt("result: #{result}")
+
+  prompt("Do you want to do another calculation?")
+  answer = gets.chomp.downcase
+  break unless answer.start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Goodbye #{name}")
