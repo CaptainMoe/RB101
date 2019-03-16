ef6b3a76deb63691ecbb6b9dbfc4ed5688f64c87
CaptainMoe <mohamedaaa1997@gmail.com>
2019-03-15 23:10:24 -0400

    lesson_2 completed

---
+++ lesson_3/Hard_1/ex01.rb
@@ -0,0 +1,5 @@
+if false
+  greeting = “hello world”
+end
+
+greeting # is nil ruby doesn't raise an error when it is not initialized in condition
---
+++ lesson_3/Hard_1/ex02.rb
@@ -0,0 +1,6 @@
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting  #  => "hi there"
+puts greetings # the object is modified in line 3
